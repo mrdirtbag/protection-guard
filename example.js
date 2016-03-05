8cdbe472d34129cce7ef2ac8dc43d48362e1f52d
Thomas Hansen <th4019@gmail.com>
2016-03-05 10:43:09 -0700

    adding example

---
+++ example.js
@@ -0,0 +1,277 @@
+var example = {
+  "petitioner": {
+    "firstName": "thomas",
+    "middleName": "seely",
+    "lastName": "hansen",
+    "homeAddress": {
+      "address": "123 lane",
+      "address2": "",
+      "city": "",
+      "state": "",
+      "zip": ""
+    },
+    "workAddress": {
+      "address": "123 lane",
+      "address2": "",
+      "city": "",
+      "state": "",
+      "zip": ""
+    },
+    "dateOfBirth": "1992-11-05",
+    "phone": "801-580-2927",
+    "othersProtected": [
+      {
+        "name": "SM",
+        "age": "12",
+        "relationship": "daughter"
+      }
+    ],
+    "attorney": {
+      "name": "John Hansen",
+      "phone": "555-555-5555"
+    }
+  },
+  "respondent": {
+    "firstName": "thomas",
+    "middleName": "seely",
+    "lastName": "hansen",
+    "otherNames": "",
+    "homeAddress": {
+      "address": "123 lane",
+      "address2": "",
+      "city": "",
+      "state": "",
+      "zip": ""
+    },
+    "relationshipToPetitioner": "",
+    "employer": "",
+    "otherAddresses": [
+      {
+        "type": "work",
+        "bestTimesToFind": "10 am to 6 pm",
+        "phone": "444-444-4444",
+        "address": "123 lane",
+        "address2": "",
+        "city": "",
+        "state": "",
+        "zip": ""
+      },
+      {
+        "type": "ufc gym",
+        "bestTimesToFind": "6-8 pm",
+        "phone": "",
+        "address": "765 cherry lane",
+        "address2": "",
+        "city": "",
+        "state": "",
+        "zip": ""
+      }
+    ],
+    "description": {
+      "sex": "female",
+      "race": "white",
+      "dateOfBirth": "1990-05-05",
+      "height": 64,
+      "weight": 135,
+      "eyes": "brown",
+      "hair": "brown",
+      "socialSecurity": 1234,
+      "distinguishingFeatures": "sleeve tattoes",
+      "driverLicense": {
+        "state": "Florida",
+        "expires": "2019-04-25"
+      }
+    },
+    "vehicle": [
+      {
+        "make": "Land Rover",
+        "model": "Range Rover",
+        "color": "pearl white",
+        "licensePlate": "BITEME",
+        "year": 2016
+      }
+    ],
+    "record": {
+      "usedWeapons": false,
+      "beenViolent": true,
+      "weaponViolenceDescription": "",
+      "probation": false,
+      "parole": false,
+      "agency": {
+        "name": "",
+        "officer": "",
+        "telephoneNumber": ""
+      }
+    }
+  },
+  "relationship": {
+    "married": true,
+    "formerlyMarried": false,
+    "dateOfDivorce": "",
+    "liveTogether": true,
+    "formerlyLivedTogether": false,
+    "relatives": false,
+    "relativeRelationship": "",
+    "expectingChild": false,
+    "childDueDate": "",
+    "children": [
+      {
+        "initials": "OE",
+        "birth": "2010-10",
+        "address": ""
+      }
+    ]
+  },
+  "recentAbuse": {
+    "when": "2016-03-01",
+    "incidentAddress": {
+      "address": "123 lane",
+      "address2": "",
+      "city": "",
+      "state": "",
+      "zip": ""
+    },
+    "police": {
+      "came": true,
+      "department": "",
+      "anyoneArrested": false,
+      "caseNumber": "",
+      "anyoneTicketed": false
+    },
+    "description": "",
+    "whyAfraid": "",
+    "weaponThreat": false,
+    "weaponDescription": "",
+    "childrenPresent": [
+      "JK",
+      "EP"
+    ],
+    "othersPresent": [
+      "sally ford"
+    ],
+    "anyoneHurt": [
+      "sally ford"
+    ],
+    "otherFacts": ""
+  },
+  "pastAbuse": {
+    "when": "2016-03-01",
+    "incidentAddress": {
+      "address": "123 lane",
+      "address2": "",
+      "city": "",
+      "state": "",
+      "zip": ""
+    },
+    "police": {
+      "came": true,
+      "department": "",
+      "anyoneArrested": false,
+      "caseNumber": "",
+      "anyoneTicketed": false
+    },
+    "description": ""
+  },
+  "otherCourtCases": {
+    "probation": {
+      "petitioner": true,
+      "respondent": true
+    },
+    "cases": [
+      {
+        "type": "custody",
+        "county": "fairfax",
+        "state": "california",
+        "caseNumber": "57643276523",
+        "personsInvolved": {
+          "petitioner": true,
+          "respondent": true
+        },
+        "judgeOrdered": true
+      }
+    ],
+    "childrenCases": [
+      {
+        "type": "custody",
+        "county": "fairfax",
+        "state": "california",
+        "court": "",
+        "caseNumber": "57643276523"
+      }
+    ]
+  },
+  "judgeRequests": {
+    "personalConduct": true,
+    "noContact": false,
+    "onlyContactForMediation": false,
+    "stayAway": {
+      "work": {
+        "address": "123 lane",
+        "address2": "",
+        "city": "",
+        "state": "",
+        "zip": ""
+      },
+      "vehicle": {
+        "make": "Chevrolet",
+        "model": "Impala",
+        "color": "Maroon",
+        "licensePlate": "214yo2",
+        "year": 2000
+      },
+      "schools": [],
+      "other": {
+        "address": "643 meadow creek",
+        "address2": "",
+        "city": "",
+        "state": "",
+        "zip": ""
+      }
+    },
+    "weaponsRestricted": [
+      "gun",
+      "firearm",
+      "knives"
+    ],
+    "propertyControl": {
+      "home": {
+        "address": "643 meadow creek",
+        "address2": "",
+        "city": "",
+        "state": "",
+        "zip": ""
+      },
+      "other": "",
+      "utilitiesAndServices": true
+    },
+    "custudyOf": [
+      "OE"
+    ],
+    "parentTime": "",
+    "whoCommunicatesParentTime": "",
+    "noAlcoholAndDrugs": true,
+    "supervisedVisitation": "",
+    "travelRestrictions": false,
+    "support": {
+      "child": 100,
+      "spousal": 0,
+      "withholding": false,
+      "childcare50": false,
+      "childMedical": true,
+      "childAbuseExpenses": 0,
+      "abuseMedicalExpenses": 0
+    },
+    "otherAssistance": "",
+    "lawEnforcement": {
+      "name": "",
+      "home": true,
+      "children": true,
+      "helpMeRemoveItems": false,
+      "helRespondentRemoveItems": true
+    },
+    "childAbuse": false,
+    "guardianAppointed": false
+  },
+  "agreement": true,
+  "signature": "Thomas Seely Hansen"
+};
