10f7181abf4dbe32d9d7ccc27c0a05caedcafe7a
Thomas Hansen <th4019@gmail.com>
2016-03-05 11:55:39 -0700

    adding control

--- client/main.js
+++ client/main.js
@@ -66,11 +66,6 @@ Accounts.ui.config({
     'click #hasOtherNames': function () {
       Session.set('hasOtherNames', !Session.get('hasOtherNames')); //toggle
     },
-    'click #step1p1': function () {
-      var order = Session.get('ProtectionOrder');
-      Session.set('ProtectionOrder', order);
-      console.log('clicked');
-    },
     'click .next': function () {
       // go to next page
       Session.set('page', Session.get('page') + 1);
@@ -80,6 +75,17 @@ Accounts.ui.config({
         Session.set('pageCount', pagesPerStep[Session.get('step')]);
         $('ul.tabs').tabs('select_tab', 'step' + Session.get('step'));
         console.log('advancing to next step');
+      }
+    },
+    'click .back': function () {
+      // go to next page
+      Session.set('page', Session.get('page') - 1);
+      if (Session.get('page') == 0) {
+        Session.set('step', Session.get('step') - 1);
+        Session.set('pageCount', pagesPerStep[Session.get('step')]);
+        Session.set('page', Session.get('pageCount'));
+        $('ul.tabs').tabs('select_tab', 'step' + Session.get('step'));
+        console.log('backing up');
       }
     }
   });
