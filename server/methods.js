4b8b2dd81dd58b15e634f69f0924acbb869ad160
Thomas Hansen <th4019@gmail.com>
2016-03-05 17:19:07 -0700

    fixes

--- server/methods.js
+++ server/methods.js
@@ -2,5 +2,9 @@ Meteor.methods({
     'request': function() {
 		request = ProtectionOrderRequests.findOne({});
 		return Handlebars.templates['request'](request);
-    }
+    },
+    'order': function() {
+		order = ProtectionOrderRequests.findOne({});
+		return Handlebars.templates['order'](order);
+    },
 });
